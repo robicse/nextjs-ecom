82b167be7fd95597a7b418cfb2bef4b8bab3b2fc
robeul <robicse8@gmail.com>
2022-10-09 01:49:42 +0600

    aaa

--- src/pages-sections/payment/PaymentForm.jsx
+++ src/pages-sections/payment/PaymentForm.jsx
@@ -1,113 +1,32 @@
 import { Box, Button, Divider, Grid, Radio, TextField } from "@mui/material";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Card1 from "components/Card1";
-import { FlexBox } from "components/flex-box";
 import { Paragraph } from "components/Typography";
-import { Formik } from "formik";
-// import { useFormik } from "formik";
 import useWindowSize from "hooks/useWindowSize";
 import useAuth from "../../hooks/useAuth";
-import Link from "next/link";
-import { useRouter } from "next/router";
 import React, { Fragment, useState } from "react";
 import * as yup from "yup";
 import axios from "axios";
-//import swal from 'sweetalert';
-import { useHistory } from 'react-router-dom';
 import { useAppContext } from "../checkout/../../contexts/AppContext";
 import cogoToast from "cogo-toast";
+import Router from "next/router";
 
 const PaymentForm = () => {
-  const { user, orderPlace } = useAuth();
+  const { user } = useAuth();
   const { state } = useAppContext();
-  console.log('user details', user);
+  // console.log('user details', user);
   const cartList = state.cart;
-  console.log('carList mini cart 4',cartList)
+  // console.log('carList mini cart 4',cartList)
   const getTotalPrice = () => {
     return cartList.reduce((accum, item) => accum + item.price * item.qty, 0);
   };
 
-  // const { handleChange, handleSubmit, errors, touched, values } = useFormik({
-  //   initialValues: initialValues,
-  //   validationSchema: checkoutSchema,
-  //   onSubmit: (values) => {
-  //     console.log('sdfsfd',values)
-  //     if (values) {
-  //       console.log('ggg')
-  //     }
-  //   },
-  // });
-
   const [paymentMethod, setPaymentMethod] = useState("cod");
   const width = useWindowSize();
-  const router = useRouter();
-  const isMobile = width < 769;
-
-  // console.log('11')
-  // const handleFormSubmit = (e) => {
-  //   console.log('value1', 'kk')
-    //const res2 = await updateCustomerProfile(value);
-    // if (res2) {
-    //   await getAllHandleFetch();
-    //   handleEditAddressForm(null);
-    // }
-  // };
-  // console.log('22')
 
   const handlePaymentMethodChange = ({ target: { name } }) => {
     setPaymentMethod(name);
   };
-
-  
-  // const handleSubmit =  () => {
-  //   console.log('aaa');
-    // const res = await orderPlace(values);
-    // if (res) {
-    //   console.log('1',res)
-    // }else{
-    //   console.log('2')
-    // }
-  // };
-
-
-
-
-
-  //const history = useHistory();
-  // if(!localStorage.getItem('auth_token')){
-  //     history.push('/');
-  //     swal("Warning","Login to goto Cart Page","error");
-  // }
-  
-  const [loading, setLoading] = useState(true);
-  // const { cartList } = useAuth();
-  // var totalCartPrice = 0;
-
-//   useEffect(() => {
-
-//     let isMounted = true;
-
-//     axios.get(`/api/cart`).then(res=>{
-//         if(isMounted)
-//         {
-//             if(res.data.status === 200)
-//             {
-//                 setCart(res.data.cart);
-//                 setLoading(false);
-//             }
-//             else if(res.data.status === 401)
-//             {
-//                 history.push('/');
-//                 swal("Warning",res.data.message,"error");
-//             }
-//         }
-//     }); 
-
-//     return () => {
-//         isMounted = false
-//     };
-// }, [history]);
-
 
   const config = {
     headers: { 
@@ -120,25 +39,10 @@ const PaymentForm = () => {
     e.preventDefault();
     console.log('ddd');
 
-  //   const shipping_address = {
-  //     name: "Test",
-  //     phone: "01700000000",
-  //     email: "test@gmail.com",
-  //     address: "Mirpur",
-  //     country:"Bangladesh",
-  //     city: "Dhaka",
-  //     state: "Dhaka",
-  //     postal_code: "5300",
-  //     checkout_type:"logged",
-  //     payment_mode: "cod",
-  // };
-
-
     var data = {
         user_id: user.id,
         getTotalPrice: getTotalPrice(),
         cartData: cartList,
-        //shipping_address: shipping_address,
         shipping_address_id: 1,
     }
 
@@ -148,14 +52,17 @@ const PaymentForm = () => {
             data,
             config
             ).then(res=>{
-                if(res.data.status === 200)
+              console.log('sss', res)
+                if(res)
                 {
-                  cogoToast.success(`${res.data?.msg}`, { position: 'top-right', bar: { size: '10px' } });
-                  history.push('/order');
+                  console.log('ddd')
+                  cogoToast.success(`${res.data?.message}`, { position: 'top-right', bar: { size: '10px' } });
+                  Router.push("/orders");
                 }
                 else
                 {
-                  cogoToast.error(`${res?.data?.response}`, {
+                  console.log('fff')
+                  cogoToast.error(`${res?.data?.message}`, {
                     position: "top-right",
                     bar: { size: "10px" },
                   });
